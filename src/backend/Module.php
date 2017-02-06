9e2440c9a181ebae1d2e4ca7a7b6573932ee3803
Philipp Frenzel <pf@com-x-cha.com>
2017-02-06 22:48:04 +0100

    Update Module.php

--- src/backend/Module.php
+++ src/backend/Module.php
@@ -7,6 +7,7 @@ use yii\base\Module as BaseModule;
 /**
  * @author Alexey Samoylov <alexey.samoylov@gmail.com>
  * @author Valentin Konusov <rlng-krsk@yandex.ru>
+ * @author Philipp Frenzel <philipp@frenzel.net>
  *
  * Class Module
  * @package net\frenzel\email\backend
@@ -28,8 +29,8 @@ class Module extends BaseModule
         if ($this->userIdentityClass === null) {
             $this->userIdentityClass = \Yii::$app->getUser()->identityClass;
         }
-        if (Yii::$app instanceof \yii\console\Application) {
-            $this->controllerNamespace = 'net\frenzel\email\backend\controllers';
+        if (\Yii::$app instanceof \yii\console\Application) {
+            $this->controllerNamespace = 'vendor\frenzelgmbh\yii2-email-manager\src\email\backend\controllers';
         }
     }
 }
